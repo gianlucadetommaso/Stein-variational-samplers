7cad641df2ed9a2992584dd1ec4a5f28b21fcbff
gianlucadetommaso <32386694+gianlucadetommaso@users.noreply.github.com>
2018-06-12 10:35:11 +0100

    Delete grad_mlpt.m

--- library/grad_mlpt.m
+++
@@ -1,8 +0,0 @@
-%% Gradient of negative log-posterior
-%
-% By Gianluca Detommaso -- 18/05/2018
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
-
-function g_mlpt = grad_mlpt(x, Fx, model, prior, obs)
-
-g_mlpt = prior.C0i*(x - prior.m0) + grad_mllkd(x, Fx, model, obs); 
