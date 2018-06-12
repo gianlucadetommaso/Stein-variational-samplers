21410940ea258812a2d4fda1e616e7a58c703a48
gianlucadetommaso <32386694+gianlucadetommaso@users.noreply.github.com>
2018-06-12 10:35:03 +0100

    Delete grad_mllkd.m

--- library/grad_mllkd.m
+++
@@ -1,8 +0,0 @@
-%% Gradient of negative log-likelihood
-%
-% By Gianluca Detommaso -- 18/05/2018
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
-
-function g_mllkd = grad_mllkd(x, Fx, model, obs)
-
-g_mllkd = sum( dFdx(x, model)' * (Fx - obs.y) , 2 ) / obs.std2;
