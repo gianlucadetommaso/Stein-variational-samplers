a63a096fd1ed43c391f1976ae6e874bf9d102186
gianlucadetommaso <32386694+gianlucadetommaso@users.noreply.github.com>
2018-06-12 10:57:06 +0100

    Add files via upload

--- library/grad_mlpt.m
+++ library/grad_mlpt.m
@@ -3,6 +3,6 @@
 % By Gianluca Detommaso -- 18/05/2018
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 
-function g_mlpt = grad_mlpt(x, Fx, J, model, prior, obs)
+function g_mlpt = grad_mlpt(x, Fx, J, prior, obs)
 
-g_mlpt = prior.C0i*(x - prior.m0) + grad_mllkd(x, Fx, J, model, obs); 
+g_mlpt = prior.C0i*(x - prior.m0) + grad_mllkd(Fx, J, obs); 
